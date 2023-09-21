ace02d65ec8a97e3fe9c9f48b5b400d10ba018fe
Keran Yang <yangkr920208@gmail.com>
2023-09-21 10:31:13 -0400

    make fields required
    When a field is not specified as required, by default the generated schema treats it as optional. In such case, when converting the schema back to struct, tools like go-jsonschema will make the field as a pointer type. Hence, a plain string field becomes *string, which is in-consistent. Adding the `required` tag fix the problem.
    
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # nba-player-go
 
-This repository is a proof of concept on how to use swagger to generate schema file from a go struct.
+This repository is a proof of concept on how to use [jsonschema](https://github.com/invopop/jsonschema) to generate schema file from a go struct.
 
-Run the following command to generate a `player.json` schema file under the `swagger-schema` folder.
+Run the following command to generate a `player.json` schema file under the `schema` folder.
 
 ```
-make swagger-gen
+make gen-schema
 ```
 
--- player/player.go
+++ player/player.go
@@ -1,14 +1,14 @@
 package player
 
 type School struct {
-	Name string `json:"name,omitempty"`
-	City string `json:"city,omitempty"`
+	Name string `json:"name" jsonschema:"required"`
+	City string `json:"city" jsonschema:"required"`
 }
 
 type Player struct {
-	Name     string  `json:"name,omitempty"`
-	Team     string  `json:"team,omitempty"`
-	Position string  `json:"position,omitempty"`
-	Number   int32   `json:"number,omitempty"`
-	School   *School `json:"school,omitempty"`
+	Name     string  `json:"name" jsonschema:"required"`
+	Team     string  `json:"team" jsonschema:"required"`
+	Position string  `json:"position" jsonschema:"required"`
+	Number   int32   `json:"number" jsonschema:"required"`
+	School   *School `json:"school" jsonschema:"required"`
 }
