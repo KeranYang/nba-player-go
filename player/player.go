fe3a15ae3a3849f98f77a727b0b5400b2c7792b8
Keran Yang <yangkr920208@gmail.com>
2023-09-21 14:17:09 -0400

    support description
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- player/player.go
+++ player/player.go
@@ -1,14 +1,14 @@
 package player
 
 type School struct {
-	Name string `json:"name" jsonschema:"required"`
-	City string `json:"city" jsonschema:"required"`
+	Name string `json:"name" jsonschema:"required,description=School name"`
+	City string `json:"city" jsonschema:"required,description=School city"`
 }
 
 type Player struct {
-	Name     string  `json:"name" jsonschema:"required"`
-	Team     string  `json:"team" jsonschema:"required"`
-	Position string  `json:"position" jsonschema:"required"`
-	Number   int32   `json:"number" jsonschema:"required"`
-	School   *School `json:"school" jsonschema:"required"`
+	Name     string  `json:"name" jsonschema:"required,description=Player name"`
+	Team     string  `json:"team" jsonschema:"required,description=Player team"`
+	Position string  `json:"position" jsonschema:"required,description=Player position"`
+	Number   int32   `json:"number" jsonschema:"required,description=Player number"`
+	School   *School `json:"school" jsonschema:"required,description=Player school"`
 }
